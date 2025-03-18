5e83e403a257a5f2ddab2acb834af516096dd422
Aditi Patil <aditiprakashpatil7@gmail.com>
2025-03-18 07:10:49 +0530

    initialized

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Chatbot from "./components/Chatbot";
 
 function App() {
   return (
-      <div className="min-h-screen w-full flex justify-center items-center bg-gray-100">
+    <div className="h-[100vh] w-screen flex justify-center items-center bg-gray-100 ">
+      <div className="w-[80vw] max-w-xl bg-white shadow-lg rounded-lg ">
         <Chatbot />
       </div>
+    </div>
   );
 }
 
